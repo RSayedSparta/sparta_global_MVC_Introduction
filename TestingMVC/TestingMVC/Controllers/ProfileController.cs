646b15caaf3f3c4faaaf6f4176edf4ab0dbc586d
rahib <rsayed@spartaglobal.com>
2019-03-12 09:27:05 +0000

    one link working

---
+++ TestingMVC/TestingMVC/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingMVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutMe()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+    }
+}
